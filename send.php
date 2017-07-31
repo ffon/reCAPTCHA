ae79490ab4b20d00cf4ecfb2fbcbf41910eacda6
ffon <poty1900@hotmail.com>
2017-07-31 16:42:27 +0700

    Create send.php

---
+++ send.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+require("dbconnect.php");
+ 
+$captcha;       
+if(isset($_POST['g-recaptcha-response'])){
+$captcha=$_POST['g-recaptcha-response'];
+$response=json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=your_secretkey&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']), true);        
+}
+if(isset($_POST['btn_submit'])  && $response['success'] == true){ // เมื่อคลิกที่กที่ปุ่ม 
+    /// ทำคำสั่งเพิ่มข้อมูลหรืออื่นๆ
+}
+?>
