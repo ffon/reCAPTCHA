13cbc6fe41a84084f430a162cd0c28c246740a51
ffon <poty1900@hotmail.com>
2017-07-31 16:44:38 +0700

    Update send.php

--- send.php
+++ send.php
@@ -5,7 +5,7 @@ require("dbconnect.php");
 $captcha;       
 if(isset($_POST['g-recaptcha-response'])){
 $captcha=$_POST['g-recaptcha-response'];
-$response=json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=your_secretkey&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']), true);        
+$response=json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6LecISsUAAAAAIBtCpX9DC5N8pi2kauclJ66ZLsd&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']), true);        
 }
 if(isset($_POST['btn_submit'])  && $response['success'] == true){ // เมื่อคลิกที่กที่ปุ่ม 
     /// ทำคำสั่งเพิ่มข้อมูลหรืออื่นๆ
