0c5986a062415a0950f368948f2df4076e5cbbb8
ffon <poty1900@hotmail.com>
2017-07-31 16:52:41 +0700

    Update send.php

--- send.php
+++ send.php
@@ -1,12 +1,12 @@
 <?php
-echo "4";
+echo "1";
 session_start();
-require("dbconnect.php");
- 
+//require("dbconnect.php");
+
 $captcha;       
 if(isset($_POST['g-recaptcha-response'])){
 $captcha=$_POST['g-recaptcha-response'];
-$response=json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6LecISsUAAAAAIBtCpX9DC5N8pi2kauclJ66ZLsd&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']), true);        
+$response=json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6Ld8ICsUAAAAALru-I7htTw3G3DGy3514y3WkAsP&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']), true);        
 }
 if(isset($_POST['btn_submit'])  && $response['success'] == true){ // เมื่อคลิกที่กที่ปุ่ม 
     /// ทำคำสั่งเพิ่มข้อมูลหรืออื่นๆ
